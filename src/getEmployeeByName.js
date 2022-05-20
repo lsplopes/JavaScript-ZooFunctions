3ed29e0be321e297e42ce6507ac7024bb3563ad3
LucasLopes <lopes.sp.lucas@gmail.com>
2022-05-20 20:17:46 -0300

    resolve requisito 4

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,13 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const getEmployee = data.employees
+    .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
+  if (employeeName === undefined) {
+    return {};
+  }
+  return getEmployee;
 }
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,29 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  return data.employees
+    .some(({ managers }) => managers
+      .includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+
+  if (isManager(managerId) === false) {
+    console.log('deu certo');
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else if (isManager(managerId) === true) {
+    const getSubordinateds = data.employees
+      .filter(({ managers }) => managers
+        .includes(managerId));
+
+    const getName = getSubordinateds.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+
+    return getName;
+  }
 }
+
+getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992');
+// console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
