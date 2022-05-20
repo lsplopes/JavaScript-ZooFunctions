419c651db90a50c0ee0f9bf69a2178e6d4c45d13
LucasLopes <lopes.sp.lucas@gmail.com>
2022-05-20 18:39:05 -0300

    resolve requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
+function getAnimalsOlderThan(animal, age2) {
   // seu código aqui
+  const getanimal = data.species.find((especies) => especies.name === animal);
+  const checkage = getanimal.residents
+    .map((elemento) => elemento.age)
+    .every((elemento) => elemento >= age2);
+  return checkage;
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,18 +2,18 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-  // const filtroDeEspecie =  data.species.filter((especie) => especie.id === ids);
-  const filtrarporId = ids.map((id) => data.species.filter((especie) => especie.id === id))
-  const filtroarrumado = (elemento) => {
-    let result = []
-    for (let index in elemento) {
-      
-      result.push(elemento[index][0]);
-    }
-    return result
-  }
+  const filtrarporId = ids.map((id) => data.species.filter((especie) => especie.id === id));
+  // const filtroarrumado = (elemento) => {
+  //   const result = [];
+  //   for (const index in elemento) {
+  //     result.push(elemento[index][0]);
+  //   }
+  //   return result;
+  // };
+  
   return filtroarrumado(filtrarporId);
 }
+
 console.log(getSpeciesByIds(
   '0938aa23-f153-4937-9f88-4858b24d6bce',
   'e8481c1d-42ea-4610-8e11-1752cfc05a46',
