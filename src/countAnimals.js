4630d4fa8fd388401cc2fb6cc257a822fabd7d74
LucasLopes <lopes.sp.lucas@gmail.com>
2022-05-21 08:01:29 -0300

    corrige lint do exercicio 1

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
+const animalGeneralCount = () => {
+  const result = data.species.reduce((objeto, current) => {
+    const { name, residents } = current;
+    return {
+      ...objeto,
+      [name]: residents.length,
+    };
+  }, {});
+  return result;
+};
+
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    return animalGeneralCount();
+  }
+  const contadorDeEspecie = data.species.filter(({ name }) => name === animal.specie);
+  const contadorPorSex = contadorDeEspecie[0].residents.filter(({ sex }) => sex === animal.sex);
+  if (animal.specie !== undefined && animal.sex !== undefined) {
+    return contadorPorSex.length;
+  } if (animal.specie !== undefined && animal.sex === undefined) {
+    return contadorDeEspecie[0].residents.length;
+  }
 }
 
 module.exports = countAnimals;
