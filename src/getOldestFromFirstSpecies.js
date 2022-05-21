d3456cb2b6c0a932e064463f513d9b7900b26fee
LucasLopes <lopes.sp.lucas@gmail.com>
2022-05-21 14:00:16 -0300

    resolve requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  // RECUPERA ARRAY IDS DOS ANIMAIS AOS QUAIS É RESPONSAVEL
+  const getEmployee = data.employees.find((idEmployee) => idEmployee.id === id).responsibleFor;
+  // RECUPERA AS INFORMAÇÕES DO PRIMEIRO ID ENCONTRADO
+  const animalResp = data.species.find((animal) => animal.id === getEmployee[0]);
+  // RECUPERA O ANIMAL MAIS VELHO DA ESPECIE
+  const orderAnimal = animalResp.residents
+    .reduce((older, current) => ((older > current.age) ? older : current.age), 0);
+  const oldestData = animalResp.residents.find((residente) => residente.age === orderAnimal);
+  return [oldestData.name, oldestData.sex, oldestData.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
