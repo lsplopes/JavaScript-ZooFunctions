ea0dbdd6a295926342a4c2f15d8923d97c77f81e
LucasLopes <lopes.sp.lucas@gmail.com>
2022-05-23 15:03:31 -0300

    apaga pasta coverage para testar se o course aceita

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,5 +1,19 @@
 const data = require('../data/zoo_data');
 
+const zones = {
+  NE: [],
+  NW: [],
+  SE: [],
+  SW: [],
+}
+
+const getSpeciesByZone = () => {
+  return Object.keys(zones).forEach((zona) => zones[zona].push(data.species.filter((animal) => animal.location === zona).name));
+
+}
+getSpeciesByZone()
+console.log(zones);
+
 function getAnimalMap(options) {
   // seu código aqui
 }
