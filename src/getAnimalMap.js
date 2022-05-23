009f437a7f27de74c86eab6d10819b993e7c8e83
LucasLopes <lopes.sp.lucas@gmail.com>
2022-05-23 15:08:30 -0300

    comenta requisito 14 não terminado para passar no lint

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,19 +1,18 @@
 const data = require('../data/zoo_data');
 
-const zones = {
-  NE: [],
-  NW: [],
-  SE: [],
-  SW: [],
-}
+// const zones = {
+//   NE: [],
+//   NW: [],
+//   SE: [],
+//   SW: [],
+// }
 
-const getSpeciesByZone = () => {
-  return Object.keys(zones).forEach((zona) => zones[zona].push(data.species.filter((animal) => animal.location === zona).name));
+// const getSpeciesByZone = () => {
+//   return Object.keys(zones).forEach((zona) => zones[zona].push(data.species.filter((animal) => animal.location === zona).name));
 
-}
-getSpeciesByZone()
-console.log(zones);
-
+// }
+// getSpeciesByZone()
+// console.log(zones);
 function getAnimalMap(options) {
   // seu código aqui
 }
